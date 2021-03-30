26acb8e3aa2867c46526fe911e2876d763a96d4f
Wesley <chqzy99@gmail.com>
2021-03-29 23:32:02 -0700

    Create SinglyLinkedList.py
    SinglyLinkedList

---
+++ SinglyLinkedList.py
@@ -0,0 +1,52 @@
+class Node:
+    def __init__(self, value=None):
+        self.value = value
+        self.next = None
+
+class SLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            node = node.next
+    # insert in Linked List
+    def insertSLL(self, value, location):
+        newNode = Node(value)
+        if self.head is None:
+            self.head = newNode
+            self.tail = newNode
+        else:
+            if location == 0:
+                newNode.next = self.head
+                self.head = newNode
+            elif location == 1:
+                newNode.next = None
+                self.tail.next = newNode
+                self.tail = newNode
+            else:
+                tempNode = self.head
+                index = 0
+                while index < location - 1:
+                    tempNode = tempNode.next
+                    index += 1
+                nextNode = tempNode.next
+                tempNode.next = newNode
+                newNode.next = nextNode
+
+
+singlyLinkedList = SLinkedList()
+singlyLinkedList.insertSLL(1, 1)
+singlyLinkedList.insertSLL(2, 1)
+singlyLinkedList.insertSLL(3, 1)
+singlyLinkedList.insertSLL(4, 1)
+# insert at the beginning
+singlyLinkedList.insertSLL(0, 0)
+# insert in the middle
+singlyLinkedList.insertSLL(3, 3)
+# insert at the end
+singlyLinkedList.insertSLL(5, 1)
+
+print([node.value for node in singlyLinkedList])
