00643d04b1320da73652587ae7fa87fabc9a782a
Wesley <chqzy99@gmail.com>
2021-04-06 14:24:12 -0700

    Create eL387. First Unique Character in a String.py

---
+++ List/eL387. First Unique Character in a String.py
@@ -0,0 +1,21 @@
+# First Non-Repeating Integer in a list:
+#Problem Statement
+#Implement a function, find_first_unique(lst) that returns the first unique integer in the list.
+
+#Input #
+#A list of integers
+
+#Output #
+#The first unique element in the list
+
+#Sample Input
+#[9,2,3,2,6,6]
+#Sample Output
+#9
+
+#Start with Ingeter, feel like there is no such thing like this in Python:
+# for (i = 0 ; i < xxx ; i++)
+#    for (j = i + 1 ; j < xxx; j++)
+
+def find_fist_unique(lst):
+    #WIP
