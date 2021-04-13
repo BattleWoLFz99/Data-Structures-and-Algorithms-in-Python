0a18e55018418e2b03dae85632d8a553fa6000a2
Wesley <chqzy99@gmail.com>
2021-04-12 21:27:13 -0700

    04122021

--- List/eL27 Remove Element.py
+++ List/eL27 Remove Element.py
@@ -4,3 +4,18 @@ class Solution:
         while val in nums:
             nums.remove(val)
         return len(nums)
+
+# Using pop()
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        # write your code here
+        item = 0
+        last = len(nums)-1
+        while (last>=item):
+            if nums[item] == val:
+                nums[item],nums[last]=nums[last],nums[item]
+                last -= 1
+                nums.pop()
+            else:
+                item += 1
+        return item
